9b253a5049a5e48cf6e2fa10e0dbb00fce119bce
Jawan Higgins <Jhiggin3@ggc.edu>
2017-08-06 10:36:19 -0400

    Set background opacity

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,11 +1,14 @@
 /* Open when someone clicks on the span element */
 $('body').on('click', '.openBtn', function() {
     document.getElementById("myNav").style.width = "100%";
+    $(".body-container").css("opacity", "0");
+
 });
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 $('body').on('click', '.closeBtn', function() {
     document.getElementById("myNav").style.width = "0%";
+    $(".body-container").css("opacity", "1");
 });
 
 
